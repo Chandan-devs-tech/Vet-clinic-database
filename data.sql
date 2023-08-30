9a9583b6ce66ba2776b2185a43cc2c2111cbbdcc
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-08-30 22:06:13 +0530

    Updated data.sql file

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(id,name,date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1
+,'Agumon','2020-2-3',0,TRUE,10.23);
+INSERT INTO animals(id,name,date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2,'Gabumon','2018-11-15',2,TRUE,8),(3,'Pikachu','2021-1-7',1,FALSE,15.04),(4,'Devimon','2017-5-12',5,TRUE,11);
+
