b850a834010c91fd703c73384e280e109ab52c47
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-09-01 02:32:34 +0530

    Updated queries.sql file

--- queries.sql
+++ queries.sql
@@ -8,3 +8,65 @@ SELECT name,escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/*Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.*/
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+/*Inside a transaction:
+Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+Verify that changes were made.
+Commit the transaction.
+Verify that changes persist after commit.*/
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE ('%mon');
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+/*Inside a transaction delete all records in the animals table, then roll back the transaction.
+After the rollback verify if all records in the animals table still exists.)*/
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+/*Inside a transaction:
+Delete all animals born after Jan 1st, 2022.
+Create a savepoint for the transaction.
+Update all animals' weight to be their weight multiplied by -1.
+Rollback to the savepoint
+Update all animals' weights that are negative to be their weight multiplied by -1.
+Commit transaction*/
+BEGIN;
+DELETE FROM animals WHERE date_of_birth >'2022-01-01';
+SAVEPOINT sp1;
+UPDATE animals SET weight_kg = weight_kg*-1;
+ROLLBACK TO sp1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+/*How many animals are there?*/
+SELECT COUNT(*) FROM animals;
+
+/*How many animals have never tried to escape?*/
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+/*What is the average weight of animals?*/
+SELECT avg(weight_kg) FROM animals;
+
+/*Who escapes the most, neutered or not neutered animals?*/
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+/*What is the minimum and maximum weight of each type of animal?*/
+SELECT species, MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, MIN(weight_kg) FROM animals GROUP BY species;
+
+/*What is the average number of escape attempts per animal type of those born between 1990 and 2000?*/
+ SELECT species, avg(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
